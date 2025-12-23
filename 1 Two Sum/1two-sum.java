44620e60bed50325431f5d79911dc4c6bf400bde
Ayush-Raj-510 <ayushpatna69@gmail.com>
2025-12-23 05:26:15 +0000

    Code challenge submission for question: 1 Two Sum

---
+++ 1 Two Sum/1two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       
+       for(int i = 1 ; i < nums.length; i++){
+        for(int j = 0 ; j <  nums.length - i; j++){
+            if(nums[j] + nums[j+i] == target)
+            {
+                return new int[]{j,j + i};
+            }
+        }
+       }
+    return new int[]{};
+    }
+}
