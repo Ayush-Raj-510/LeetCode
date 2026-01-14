76f07eb629bbd0ea07adbfb95da5d3e512e27b85
Ayush-Raj-510 <ayushpatna69@gmail.com>
2026-01-14 05:26:59 +0000

    Code challenge submission for question: 13 Roman to Integer

---
+++ 13 Roman to Integer/13roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        if (s == null || s.length() == 0) return 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length() - 1));
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+            int next = map.get(s.charAt(i + 1));
+
+            result += (curr < next) ? -curr : curr;
+        }
+
+        return result;
+    }
+}
