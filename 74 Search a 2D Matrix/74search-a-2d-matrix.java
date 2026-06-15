5cafc76b324bc8ca3c25e8befce255bc3220cc0d
Ayush-Raj-510 <ayushpatna69@gmail.com>
2026-06-15 06:10:32 +0000

    Code challenge submission for question: 74 Search a 2D Matrix

---
+++ 74 Search a 2D Matrix/74search-a-2d-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length; // no. of rows
+        int n=matrix[0].length; // no.of cols
+        int low=0;
+        int high=m*n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/n;
+            int col=mid%n;
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
